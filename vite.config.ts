0ea73e8418369e0027ae6075a7adc0ad4c4387c3
Kevin <Kevinthiombiano340@gmail.com>
2026-02-03 14:31:57 +0000

    chore: init project

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@features": path.resolve(__dirname, "./src/features"),
+      "@shared": path.resolve(__dirname, "./src/shared"),
+      "@ui": path.resolve(__dirname, "./src/shared/components/ui"),
+    },
+  },
+  server: {
+    host: true,
+  },
+});
